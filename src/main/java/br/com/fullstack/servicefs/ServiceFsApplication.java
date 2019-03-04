1c9547523e6945476723ed6889e70d0845517a36
kito koioto <kitokoioto@gmail.com>
2019-03-04 09:03:37 -0300

    Start boot

---
+++ src/main/java/br/com/fullstack/servicefs/ServiceFsApplication.java
@@ -0,0 +1,13 @@
+package br.com.fullstack.servicefs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceFsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceFsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/fullstack/servicefs/ServiceFsApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.fullstack.servicefs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ServiceFsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
